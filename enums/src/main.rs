b8c1b05607bfbedbff2c1c02eb7547340d154fd8
Michael-Evangelos Diamantis <michaeldiamantis17@gmail.com>
2026-07-17 19:01:45 +0300

    Collections and enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,19 @@
-enum Color {
-    Red,
-    Green,
-    Blue,
-}
+use std::{fs::File, io::Read};
 
-enum Custom<T> {
-    Some(T),
-    None,
+#[must_use]
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
 
 fn main() {
-    let color = Color::Red;
+    let res = File::open("foo");
+    match res {
+        Ok(f) => {
+            println!("OK");
+        }
+        Err(e) => {
+            println!("ERROR");
+        }
+    }
 }
